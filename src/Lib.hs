c335adf2c094ce01780dd8257be382b1da2180b2
Ilya Smelkov <triplepointfive@gmail.com>
2015-09-16 23:25:40 +0300

    Parse dir tree

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,19 +2,33 @@ module Lib
     ( someFunc
     ) where
 
+import qualified Data.Map.Strict as Map
+
 import Language.Haskell.Meta
 import Language.Haskell.Exts.Syntax
+import System.Directory.Tree
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-listFileImports :: FilePath -> IO [String]
-listFileImports filePath = do
-  a <- parseHsModule <$> readFile filePath
-  case a of
-    Right m -> return (moduleImports m)
-    Left  s -> putStrLn s >> return []
+type Node = (String, [String])
+type Tree = Map.Map String [String]
 
-moduleImports :: Module -> [String]
-moduleImports (Module _ _ _ _ _ imp _) =
-    (\ (ModuleName x) -> x) <$> importModule <$> imp
+buildDirTree :: FilePath -> IO Tree
+buildDirTree filePath = do
+    (_ :/ at) <- readDirectory filePath
+    return (foldDirTree Map.empty at)
+
+foldDirTree :: Tree -> DirTree String -> Tree
+foldDirTree tree (File _ content) = case parseHsModule content of
+    Right m -> addNode (moduleNode m) tree
+    Left  s -> tree -- Just for now
+foldDirTree tree (Dir _ nodes) = foldl foldDirTree tree nodes
+foldDirTree tree (Failed _ _)  = tree
+
+addNode :: Node -> Tree -> Tree
+addNode (key, val) = Map.insert key val
+
+moduleNode :: Module -> Node
+moduleNode (Module _ (ModuleName modName) _ _ _ imp _) =
+    (modName, (\ (ModuleName x) -> x) <$> importModule <$> imp)
